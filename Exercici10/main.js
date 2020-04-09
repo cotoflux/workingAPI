86de77778708fbcafe7fabdc6b5ecc0f86df37d9
cotoflux <guaupelu@hotmail.com>
2020-04-09 16:14:53 +0200

    Borrado variable comentada que no tenia sentido

--- Exercici10/main.js
+++ Exercici10/main.js
@@ -1,7 +1,6 @@
 function navidad(){
 
     var numero = parseInt(document.getElementById("numero").value);
-   // var numeroMenos=numero-1
 
     for(i=0; i <= numero; i++){
         for(j=1;  j < i; j++){
