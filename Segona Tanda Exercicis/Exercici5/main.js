17c70bbfa40b32aaa692c27fce5bd0ed529b91b8
cotoflux <guaupelu@hotmail.com>
2020-04-14 17:41:10 +0200

    Rehecho ejercicio 5

--- Segona Tanda Exercicis/Exercici5/main.js
+++ Segona Tanda Exercicis/Exercici5/main.js
@@ -4,13 +4,16 @@ var numero = parseInt(document.getElementById("numero").value);
 
 
 
-    if(numero%2 == 0){
-        document.getElementById("primer").innerHTML="El número introducido es primer";
-    } else if(numero%2 != 0){
-        document.getElementById("primer").innerHTML="El número introducido no es primer";
+    if((numero>5) && (numero%2 == 0) || (numero%3 == 0) || (numero%5 == 0)){
+        document.getElementById("primer").innerHTML="El número introduit no es primer";
+    } else if(numero==4){
+        document.getElementById("primer").innerHTML="El número introduit no es primer";
+    } else if ((numero>5) && (numero%2 != 0) || (numero3 != 0) || (numero%5 != 0)){
+        document.getElementById("primer").innerHTML="El número introduit es primer";
     } else {
-        document.getElementById("primer").innerHTML="El número introducido es incorrecto";
+        document.getElementById("primer").innerHTML="Siusplau introdueix un número";
     }
+    
     
 
 }
