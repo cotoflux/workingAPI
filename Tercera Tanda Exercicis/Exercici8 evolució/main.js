6220a04d1cc1138cdb37966e1f4258d3b8624358
cotoflux <guaupelu@hotmail.com>
2020-04-21 17:44:53 +0200

    Finalizado el Exercici 8 evolucion

--- Tercera Tanda Exercicis/Exercici8 evolució/main.js
+++ Tercera Tanda Exercicis/Exercici8 evolució/main.js
@@ -1,16 +1,15 @@
 function calculaAny(){
 
-let any = document.getElementById("any").value;
-var obtenFecha = new Date('January 1, ' + any + ' 23:15:30');
-var numeroDia = obtenFecha.getDay();
-var diaSemana = numeroDia;
+let any = 2000
 
-    if(diaSemana == 0){
-        document.getElementById("resposta").innerHTML = "Si es domingo";
-    } else if (diaSemana == 1 || diaSemana ==2 || diaSemana==3 || diaSemana==4 || diaSemana==5 | diaSemana==6){
-        document.getElementById("resposta").innerHTML = "No es domingo";
-    } else {
-        document.getElementById("resposta").innerHTML = "El dato introducido no es correcto";
+    for(i=any; any<=2100; i++){
+        var obtenFecha = new Date('January 1, ' + any + ' 23:15:30');
+        var numeroDia = obtenFecha.getDay();
+        var diaSemana = numeroDia;
+        if(diaSemana == 0){
+         document.getElementById("resposta").innerHTML +=  "El 1 de Enero del año " + any + "si es domingo<br>";
+        }
+        any++
     }
 
 }
